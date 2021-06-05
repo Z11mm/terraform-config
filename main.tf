9501198451a763f287e8a0c22ed7dd0782d68341
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-06-05 14:35:55 +0100

    Add firewall rule for web server

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # Specify cloud provider
 provider "google" {
+    credentials = "${file("my-startup-project-281916-7c26344b85ac.json")}"
     project = "my-startup-project-281916"
     region = "us-central1"
 }
@@ -10,8 +11,15 @@ resource "google_compute_instance" "default" {
   machine_type = "e2-medium"
   zone = "us-central1-a"
 
+#  Apply the firewall rule to allow external IPs to access this instance
+  tags = [ "http-server" ]
+
   network_interface {
     network = "default"
+
+    access_config {
+    #   Include this block to give the VM a public IP address
+    }
   }
 
   boot_disk {
@@ -21,4 +29,27 @@ resource "google_compute_instance" "default" {
         size = 10
     }
   }
+
+# Run web server on instance
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
+}
+
+# Allow incoming web traffic to instance
+resource "google_compute_firewall" "default" {
+  name = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["8080"]
+  }
+
+  source_ranges = [ "0.0.0.0/0" ]
+  target_tags = [ "http-server" ]
+
+  direction = "INGRESS"
+}
+
+output "public ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
