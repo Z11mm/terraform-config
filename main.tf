7889476b443eb245fa491f92c1fcb66497889a50
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-06-05 18:27:57 +0100

    Chore:Add comments

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "google_compute_firewall" "default" {
   direction = "INGRESS"
 }
 
+# Output public ip assigned after vm instance creation
 output "public_ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
